3240891ad06613bec5cad43a26a000d8715b5f66
jorgitodf <jspaiva.1977@gmail.com>
2017-05-22 22:19:33 -0300

    Aula 05 - Semana 01

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.get('/', (req, res) => {
-    return res.render('main/index', {
-        title : 'Testando Ecommerce'
-    })
+	return res.render('main/index', {
+		title: 'Test Ecommerce',
+		layout: 'layouts/base'
+	})
 })
 
-module.exports = router;
+module.exports = router
